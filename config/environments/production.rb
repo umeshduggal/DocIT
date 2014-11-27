b25a8c70180cacf167e5633a138224c0c3f9c3f2
kuldeep <kuldeep.it2008@gmail.com>
2014-11-27 12:14:49 +0530

    make changes to add configurable variable for superadmin email id

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,9 +68,11 @@ DocIT::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-   config.action_mailer.default_url_options = { host: '202.164.60.61', port: 4000 }
+  config.action_mailer.default_url_options = { host: '202.164.60.61', port: 4000 }
 
-    config.action_mailer.smtp_settings = {
+  config.super_admin_mail_id = "obgynmy@gmail.com"
+  
+  config.action_mailer.smtp_settings = {
       address: "smtp.live.com",
       port: 25,
       domain: "live.com",
